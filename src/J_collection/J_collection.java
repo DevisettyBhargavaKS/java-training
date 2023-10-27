c1bb1241d5229a33f63c587d8ad5ea304784d69b
DevisettyBhargavaKS <bhargava.devisetty@kumaran.com>
2023-10-27 16:34:24 +0530

    updated

--- src/J_collection/J_collection.java
+++ src/J_collection/J_collection.java
@@ -4,11 +4,25 @@ import java.util.ArrayList;
 
 public class J_collection {
     public static void main(String[] args) {
-        ArrayList<Integer>arrayList=new ArrayList<>();
-        arrayList.add(1);
-        arrayList.add(2);
-        arrayList.add(3);
-        System.out.println(arrayList);
+        ArrayList<student>arr=new ArrayList<>();
 
+        arr.add(new student(1,"bhargav"));
+        arr.add(new student(2,"sivanesan"));
+        arr.add(new student(3,"nani"));
+
+
+        System.out.println("*****before removeing******");
+        for (student student:arr){
+            System.out.println(student.getRollno());
+            System.out.println(student.getName());
+
+        }
+        System.out.println("*****after removeing******");
+        arr.remove(2);
+        for (student student:arr){
+            System.out.println(student.getRollno());
+            System.out.println(student.getName());
+
+        }
     }
 }
